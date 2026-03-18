2e904af51bb1bd1a5d919a952ee26ecc27ab7e8c
alan03157cc-commits <alan03157cc@gmail.com>
2026-03-19 01:20:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -125,6 +125,7 @@ function filterWarrants(warrants, mode = 'swing') {
             };
         })
         .filter(w => {
+            // 暫時放寬，讓資料先顯示
             if (mode === 'short') {
                 return w.days >= 10 && w.moneyness >= -50 && w.moneyness <= 50 && parseFloat(w.dlr_percent) <= 1.0;
             } else {
